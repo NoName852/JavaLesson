86d6cce64c68659cfcf511ee6c24a67b2c5e58ed
NoName852 <__>
2017-10-04 14:01:19 +0400

    Циклические алгоритмы

---
+++ lesson002/ZiklFor01.java
@@ -0,0 +1,18 @@
+package JavaLesson.lesson002;
+import java.util.Scanner;
+/*
+Составить программу вывода любого числа заданное количество раз
+*/
+public class ZiklFor01 {
+    public static void main(String[] args) {
+        int a, povtor;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Повторяющееся число: ");
+        a = scn.nextInt();
+        System.out.print("Количество повторений: ");
+        povtor = scn.nextInt();
+        for (int i = 0; i < povtor; i++) {
+            System.out.print(a + ", ");
+        }
+    }
+}
---
+++ lesson002/ZiklWhile01.java
@@ -0,0 +1,24 @@
+package JavaLesson.lesson002;
+import java.util.Scanner;
+/*
+Вводить с клавиатуры числа, пока сумма чисел не привысит 150.
+Вывести сумму и среднее арифметическое этих чисел
+*/
+public class ZiklWhile01 {
+    public static void main(String[] args) {
+        double chislo, sum, sr;
+        int n = 0;
+        Scanner scn = new Scanner(System.in);
+        sum = 0;
+        while (sum < 150) {
+            System.out.print("Введи число: ");
+            chislo = scn.nextDouble();
+            sum += chislo;
+            n++;
+        }
+        System.out.println("Сумма чисел превысила заданное значение");
+        sr = sum / n;
+        System.out.println("Количество чисел = " + n);
+        System.out.printf("Сумма чисел = %.2f их среднее арифметическое = %.2f", sum, sr);
+    }
+}
